c4872dd9ee53fea680260e4c9294def080c505c0
divv369 <makerslegacy604@gmail.com>
2026-08-03 03:12:30 +0530

    Time: 0 ms (100%), Space: 13.2 MB (79.85%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,25 +3,39 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m=matrix.size();
         int n=matrix[0].size();
-        int low=0 ;int high=m-1;
-        int res;
+        // solution number 1
+        // int low=0 ;int high=m-1;
+        // int res;
+        // while(low<=high){
+        //     int guess=(low+high)/2;
+        //     if(matrix[guess][0]==target) return true;
+        //     if(matrix[guess][0]<target){
+        //         res=guess;
+        //         low=guess+1;
+        //     }
+        //     else high=guess-1;
+        // }
+
+        // low=0 ; high=n-1;
+        // while(low<=high){
+        //     int guess=(low+high)/2 ;
+        //     if(matrix[res][guess]==target) return true;
+        //     if(matrix[res][guess]>target) high=guess-1;
+        //     else low=guess+1;
+        // }
+        // return false;
+        //solution 2 usaing single binary search only
+        int low=0 ;int high=n*m-1;
         while(low<=high){
             int guess=(low+high)/2;
-            if(matrix[guess][0]==target) return true;
-            if(matrix[guess][0]<target){
-                res=guess;
-                low=guess+1;
-            }
-            else high=guess-1;
-        }
-
-        low=0 ; high=n-1;
-        while(low<=high){
-            int guess=(low+high)/2 ;
-            if(matrix[res][guess]==target) return true;
-            if(matrix[res][guess]>target) high=guess-1;
-            else low=guess+1;
+            int rows=guess/n ;
+            int col=guess%n ;
+            if(matrix[rows][col]==target) return true;
+            if(matrix[rows][col]>target) high=guess-1;
+            else low=guess+1 ;
         }
         return false;
+        
+
     }
 };
